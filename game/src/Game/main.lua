77049d181f5f4ab47caeac4fd325a4b52bace146
れみろう <remyroez+github@gmail.com>
2019-07-07 21:44:59 +0900

    :rotating_light: シーン管理テスト

--- game/src/Game/main.lua
+++ game/src/Game/main.lua
@@ -6,6 +6,8 @@ local Game = require(folderOfThisFile .. 'class')
 
 -- クラス
 local Application = require 'Application'
+local EntityStack = require 'EntityStack'
+local InGame = require 'scenes.InGame'
 
 -- 初期化
 function Game:initialize(...)
@@ -14,23 +16,28 @@ end
 
 -- 読み込み
 function Game:load(...)
+    self.scene = EntityStack()
+    self.scene:add(InGame())
 end
 
 -- 更新
 function Game:update(dt, ...)
+    self.scene:update()
 end
 
 -- 描画
 function Game:draw(...)
-    love.graphics.print('Hello, Game!')
+    self.scene:draw()
 end
 
 -- キー入力
 function Game:keypressed(key, scancode, isrepeat)
+    self.scene:call('keypressed', key, scancode, isrepeat)
 end
 
 -- キー離した
 function Game:keyreleased(key, scancode)
+    self.scene:call('keyreleased', key, scancode)
 end
 
 -- テキスト入力
@@ -39,10 +46,12 @@ end
 
 -- マウス入力
 function Game:mousepressed(x, y, button, istouch, presses)
+    self.scene:call('mousepressed', x, y, button, istouch, presses)
 end
 
 -- マウス離した
 function Game:mousereleased(x, y, button, istouch, presses)
+    self.scene:call('mousereleased', x, y, button, istouch, presses)
 end
 
 -- マウス移動
