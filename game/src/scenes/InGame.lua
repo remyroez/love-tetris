dd3fc2b0a06d98fca64289e649257371acca22a5
れみろう <remyroez+github@gmail.com>
2019-07-09 20:30:33 +0900

    :pencil: インゲームシーンにエンティティマネージャ追加

--- game/src/scenes/InGame.lua
+++ game/src/scenes/InGame.lua
@@ -8,21 +8,29 @@ local Entity = require 'Entity'
 -- インゲーム クラス
 local InGame = class('InGame', Entity)
 
+-- クラス
+local EntityManager = require 'EntityManager'
+
 -- 初期化
 function InGame:initialize()
     Entity.initialize(self)
+    self.manager = EntityManager()
 end
 
 -- 破棄
 function InGame:destroy()
+    self.manager:destroy()
+    self.manager = nil
 end
 
 -- 更新
 function InGame:update(dt)
+    self.manager:update(dt)
 end
 
 -- 描画
 function InGame:draw()
+    self.manager:draw()
 end
 
 -- キー入力
